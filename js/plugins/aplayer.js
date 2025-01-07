8479f49ceb4b98672a8792f3c2caa45f48c25bdc
Int mian[] <2325415123@qq.com>
2025-01-07 23:25:45 +0800

    Site updated: 2025-01-07 23:25:44

--- js/plugins/aplayer.js
+++ js/plugins/aplayer.js
@@ -1,33 +1,41 @@
-(function() {
-  const audioList = [];
-  const isFixed = theme.plugins.aplayer.type === "fixed";
-  const isMini = theme.plugins.aplayer.type === "mini";
+(function () {
+    const audioList = [];
+    const isFixed = theme.plugins.aplayer.type === "fixed";
+    const isMini = theme.plugins.aplayer.type === "mini";
 
-  for (const audio of theme.plugins.aplayer.audios) {
-    const audioObj = {
-      name: audio.name,
-      artist: audio.artist,
-      url: audio.url,
-      cover: audio.cover,
-      lrc: audio.lrc,
-      theme: audio.theme,
-    };
-    audioList.push(audioObj);
-  }
+    for (const audio of theme.plugins.aplayer.audios) {
+        const audioObj = {
+            name: audio.name,
+            artist: audio.artist,
+            url: audio.url,
+            cover: audio.cover,
+            lrc: audio.lrc,
+            theme: audio.theme,
+        };
+        audioList.push(audioObj);
+    }
 
-  if (isMini) {
-    new APlayer({
-      container: document.getElementById("aplayer"),
-      mini: true,
-      audio: audioList,
+    if (isMini) {
+        new APlayer({
+            container: document.getElementById("aplayer"),
+            autoplay: true,
+            mini: true,
+            audio: audioList,
+        });
+    } else if (isFixed) {
+        const player = new APlayer({
+            container: document.getElementById("aplayer"),
+            autoplay: true,
+            fixed: true,
+            lrcType: 3,
+            audio: audioList,
+        });
+        // document.querySelector(".aplayer-icon-lrc").click();
+    }
+    document.addEventListener('DOMContentLoaded', (event) => {
+        const player = document.getElementById('aplayer');
+        if (player) {
+            player.click(); // 模拟用户点击
+        }
     });
-  } else if (isFixed) {
-    const player = new APlayer({
-      container: document.getElementById("aplayer"),
-      fixed: true,
-      lrcType: 3,
-      audio: audioList,
-    });
-    document.querySelector(".aplayer-icon-lrc").click();
-  }
 })();
